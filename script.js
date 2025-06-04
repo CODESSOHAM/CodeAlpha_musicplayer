9898ea4c164a979348cd810a49716f4ed77a8e04
CODESSOHAM <sohamaich06@gmail.com>
2025-06-04 23:44:55 +0530

    retract2

--- script.js
+++ script.js
@@ -79,7 +79,9 @@ prevBtn?.addEventListener('click', () => {
 nextBtn?.addEventListener('click', () => {
   if (currentPlaylist.length === 0) return;
 
- else {
+  if (isRepeat) {
+    playSong(currentSongIndex);
+  } else {
     let newIndex = currentSongIndex + 1;
     if (newIndex >= currentPlaylist.length) newIndex = 0;
     playSong(newIndex); // <-- this is already updating everything
